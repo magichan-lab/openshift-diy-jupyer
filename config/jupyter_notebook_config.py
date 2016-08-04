fff7eaa1d1b0106bf1153e2aa8b13da311f69d11
magichan <magichan.lab@gmail.com>
2016-08-04 17:16:12 +0900

    Add password settings

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 # Configuration file for jupyter-notebook.
 
-c.NotebookApp.password = 'jupyter'
+c.NotebookApp.password = 'sha1:f47378fec76d:a7411cfd32b857e9466e3fcb3cedcc2a2e7fbb57'
 c.ConnectionFileMixin.transport = 'ipc'
